80f03aef8db7c5bae8dae1740aae39bfe0277e7c
Jhansi <jhansiranimathivanan@gmail.com>
2024-01-20 01:22:22 +0530

    learning_turtlegui

---
+++ 02_Oops_coffee_machine/main.py
@@ -0,0 +1,19 @@
+# import another
+# print(another.var)
+
+# import turtle
+from turtle import Turtle, Screen
+
+#timmy is the object of the turtle class
+timmy = Turtle()
+# print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+
+
+#canvheight is the attribute of the my_screen object
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
